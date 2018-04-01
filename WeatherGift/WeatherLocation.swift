c7f2c8cf196d8e7d64077392af8f9152c2fc647c
Tram Nguyen <tramnguyen@trams-macbook-pro.local>
2018-04-01 15:51:55 -0400

    7.21

--- WeatherGift/WeatherLocation.swift
+++ WeatherGift/WeatherLocation.swift
@@ -12,6 +12,17 @@ import SwiftyJSON
 
 // this gives us app wide scope of the struct
 class WeatherLocation {
+    
+    struct DailyForecast {
+        var dailyMaxTemp: Double
+        var dailyMinTemp: Double
+        var dailyDate: Double
+        var dailySummary: String
+        var dailyIcon: String
+        
+        
+    }
+    
     var name = ""
     var coordinates = ""
     var currentTemp = "--"
@@ -19,6 +30,7 @@ class WeatherLocation {
     var currentIcon = ""
     var currentTime = 0.0
     var timeZone = ""
+    var dailyForecastArray = [DailyForecast]()
     
     
     func getWeather(completed: @escaping () -> ()) {
@@ -53,6 +65,18 @@ class WeatherLocation {
                     self.currentTime = time
                 } else {
                     print("Could not return an icon")
+                }
+                let dailyDataArray = json["daily"]["data"]
+                self.dailyForecastArray = []
+                for day in 1...dailyDataArray.count-1 {
+                    let maxTemp = json["daily"]["data"][day]["temperatureHigh"].doubleValue
+                    let minTemp = json["daily"]["data"][day]["temperatureLow"].doubleValue
+                    let dateValue = json["daily"]["data"][day]["time"].doubleValue
+                    let icon = json["daily"]["data"][day]["icon"].stringValue
+                    let dailySummary = json["daily"]["data"][day]["summary"].stringValue
+                    let newDailyForecast = DailyForecast(dailyMaxTemp: maxTemp, dailyMinTemp: minTemp, dailyDate: dateValue, dailySummary: dailySummary, dailyIcon: icon)
+                    self.dailyForecastArray.append(newDailyForecast)
+                    print("***** newDailyForecast = \(newDailyForecast)")
                 }
             case .failure(let error):
                 print(error)
